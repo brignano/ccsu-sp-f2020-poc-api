c8be910f6bfab4e1ecbbb7969ce7ce087dd429e5
Anthony Brignano <abrignano@comcast.net>
2020-08-22 10:48:34 -0400

    add flask-cors back

--- application.py
+++ application.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from apis import api
+from flask_cors import CORS
 
 application = Flask(__name__)
+CORS(application)
 api.init_app(application)
 
 if __name__ == "__main__":
